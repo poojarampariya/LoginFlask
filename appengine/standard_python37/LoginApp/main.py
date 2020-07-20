0b91acc6238841b4cd8b6ee5fd856ad4c69ff8f9
poojarampariya <47311489+poojarampariya@users.noreply.github.com>
2020-07-20 14:47:50 +0530

    Update main.py

--- appengine/standard_python37/LoginApp/main.py
+++ appengine/standard_python37/LoginApp/main.py
@@ -3,7 +3,7 @@ from flask import Flask, flash, redirect, render_template, request, session, abo
 import os
 
 app = Flask(__name__)
-app.secret_key = 'secret'
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
   
 @app.route('/')
 def home():
