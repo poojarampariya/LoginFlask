1f6e4c5839c8ef55ecc423f02655e56641f351b7
poojarampariya <47311489+poojarampariya@users.noreply.github.com>
2020-07-21 08:40:34 +0530

    Update main.py

--- appengine/standard_python37/LoginApp/main.py
+++ appengine/standard_python37/LoginApp/main.py
@@ -1,60 +1,26 @@
-from flask import (
-    Flask,
-    g,
-    redirect,
-    render_template,
-    request,
-    session,
-    url_for
-)
-
-class User:
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
-
-    def __repr__(self):
-        return f'<User: {self.username}>'
-
-users = []
-users.append(User(id=1, username='Anthony', password='password'))
-users.append(User(id=2, username='Becca', password='secret'))
-users.append(User(id=3, username='Carlos', password='somethingsimple'))
-
+from flask import Flask,request,render_template
+import pickle
 
 app = Flask(__name__)
-app.secret_key = 'somesecretkeythatonlyishouldknow'
 
-@app.before_request
-def before_request():
-    g.user = None
 
-    if 'user_id' in session:
-        user = [x for x in users if x.id == session['user_id']][0]
-        g.user = user
-        
+@app.route('/')
+def hello_world():
+    return render_template("login.html")
+database={'nachi':'123','james':'aac','karthik':'asdsf'}
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/form_login',methods=['POST','GET'])
 def login():
-    if request.method == 'POST':
-        session.pop('user_id', None)
+    name1=request.form['username']
+    pwd=request.form['password']
+    if name1 not in database:
+	    return render_template('login.html',info='Invalid User')
+    else:
+        if database[name1]!=pwd:
+            return render_template('login.html',info='Invalid Password')
+        else:
+	         return render_template('home.html',name=name1)
 
-        username = request.form['username']
-        password = request.form['password']
-        
-        user = [x for x in users if x.username == username][0]
-        if user and user.password == password:
-            session['user_id'] = user.id
-            return redirect(url_for('profile'))
-
-        return redirect(url_for('login'))
-
-    return render_template('login.html')
-
-@app.route('/profile')
-def profile():
-    if not g.user:
-        return redirect(url_for('login'))
-
-    return render_template('profile.html')
+if __name__ == '__main__':
+    app.run()
+© 2020 GitHub, Inc.
