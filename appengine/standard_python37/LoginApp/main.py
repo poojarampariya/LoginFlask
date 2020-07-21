2f394e10bb194d82daf691c1b8807463ce11de74
poojarampariya <47311489+poojarampariya@users.noreply.github.com>
2020-07-21 09:50:09 +0530

    Update main.py

--- appengine/standard_python37/LoginApp/main.py
+++ appengine/standard_python37/LoginApp/main.py
@@ -5,20 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("login.html")
+	return render_template("login.html")
 database={'joey':'123','rachel':'456','monica':'789'}
 
 @app.route('/form_login',methods=['POST','GET'])
 def login():
-    name1=request.form['username']
-    pwd=request.form['password']
-    if name1 not in database:
-	return render_template('login.html',info='Invalid User')
-    else:
-        if database[name1]!=pwd:
-            return render_template('login.html',info='Invalid Password')
+	name1=request.form['username']
+	pwd=request.form['password']
+	if name1 not in database:
+		return render_template('login.html',info='Invalid User')
+	else:
+		if database[name1]!=pwd:
+			return render_template('login.html',info='Invalid Password')
         else:
-	    return render_template('home.html',name=name1)
+		return render_template('home.html',name=name1)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+	app.run(host='127.0.0.1', port=8080, debug=True)
