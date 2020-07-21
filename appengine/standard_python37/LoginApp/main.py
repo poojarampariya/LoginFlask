f7167b12a849e95e6f3adb515f569346acc3f9f3
poojarampariya <47311489+poojarampariya@users.noreply.github.com>
2020-07-21 09:44:32 +0530

    Update main.py

--- appengine/standard_python37/LoginApp/main.py
+++ appengine/standard_python37/LoginApp/main.py
@@ -3,7 +3,6 @@ import pickle5 as pickle
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     return render_template("login.html")
@@ -14,12 +13,12 @@ def login():
     name1=request.form['username']
     pwd=request.form['password']
     if name1 not in database:
-	    return render_template('login.html',info='Invalid User')
+	return render_template('login.html',info='Invalid User')
     else:
         if database[name1]!=pwd:
             return render_template('login.html',info='Invalid Password')
         else:
-	         return render_template('home.html',name=name1)
+	    return render_template('home.html',name=name1)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
