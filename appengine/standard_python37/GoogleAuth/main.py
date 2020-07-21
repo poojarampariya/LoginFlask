a3430bfaa8efd16e2ceec46b6a36324fc0a4926a
poojarampariya <47311489+poojarampariya@users.noreply.github.com>
2020-07-21 14:53:11 +0530

    Update main.py

--- appengine/standard_python37/GoogleAuth/main.py
+++ appengine/standard_python37/GoogleAuth/main.py
@@ -1,73 +1,28 @@
-from flask import Flask, redirect, url_for, session
-from authlib.integrations.flask_client import OAuth
-import os
-from datetime import timedelta
+import datetime
 
-# decorator for routes that should be accessible only by logged in users
-from auth_decorator import login_required
+from flask import Flask, render_template
 
-
-# dotenv setup
-from dotenv import load_dotenv
-load_dotenv()
-
-
-# App config
 app = Flask(__name__)
-# Session config
-app.secret_key = os.getenv("APP_SECRET_KEY")
-app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
-
-# oAuth Setup
-oauth = OAuth(app)
-google = oauth.register(
-    name='google',
-    client_id=os.getenv("734672755212-br87tkaohmtsu24l1481qridbvodf62r.apps.googleusercontent.com"),
-    client_secret=os.getenv("ozEffvkNhszjKl_kv9-PIb0U"),
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
-    client_kwargs={'scope': 'openid email profile'},
-)
 
 
 @app.route('/')
-@login_required
-def hello_world():
-    email = dict(session)['profile']['email']
-    return f'Hello, you are logge in as {email}!'
+def root():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
 
+    return render_template('index.html', times=dummy_times)
 
-@app.route('/login')
-def login():
-    google = oauth.create_client('google')  # create the google oauth client
-    redirect_uri = url_for('authorize', _external=True)
-    return google.authorize_redirect(redirect_uri)
-
-
-@app.route('/authorize')
-def authorize():
-    google = oauth.create_client('google')  # create the google oauth client
-    token = google.authorize_access_token()  # Access token from google (needed to get user info)
-    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
-    user_info = resp.json()
-    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
-    # Here you use the profile/user data that you got and query your database find/register the user
-    # and set ur own data in the session not the profile from google
-    session['profile'] = user_info
-    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
-    return redirect('/')
-
-
-@app.route('/logout')
-def logout():
-    for key in list(session.keys()):
-        session.pop(key)
-    return redirect('/')
 
 if __name__ == '__main__':
-	app.run(host='127.0.0.1', port=8080, debug=True)
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
