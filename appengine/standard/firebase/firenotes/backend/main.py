a33114c58abfbf57519b6b3d51febfe5a25de77f
poojarampariya <47311489+poojarampariya@users.noreply.github.com>
2020-07-22 14:58:28 +0530

    Update main.py

--- appengine/standard/firebase/firenotes/backend/main.py
+++ appengine/standard/firebase/firenotes/backend/main.py
@@ -123,3 +123,6 @@ def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
+
+if __name__ == '__main__':
+	app.run(host='127.0.0.1', port=8080, debug=True)
