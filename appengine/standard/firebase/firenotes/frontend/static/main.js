5d24d47694efb4bfca069f0c94ef229217db10a8
poojarampariya <47311489+poojarampariya@users.noreply.github.com>
2020-07-23 11:30:03 +0530

    Update main.js

--- appengine/standard/firebase/firenotes/frontend/static/main.js
+++ appengine/standard/firebase/firenotes/frontend/static/main.js
@@ -17,7 +17,7 @@ $(function(){
   // deploying the application to a live production environment, change to
   // https://backend-dot-<PROJECT_ID>.appspot.com as specified in the
   // backend's app.yaml file.
-  var backendHostUrl = 'https://firebaseproject-284103.uk.r.appspot.com';
+  var backendHostUrl = 'https://backend-dot-firebaseproject-284103.uk.r.appspot.com';
 
   // [START gae_python_firenotes_config]
   // Obtain the following from the "Add Firebase to your web app" dialogue
