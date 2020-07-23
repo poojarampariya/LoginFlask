05d913c8a3829e1587f6e87f6881a35209f08718
poojarampariya <47311489+poojarampariya@users.noreply.github.com>
2020-07-23 12:01:00 +0530

    Update main.py

--- appengine/standard/firebase/firenotes/backend/main.py
+++ appengine/standard/firebase/firenotes/backend/main.py
@@ -1,17 +1,3 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import logging
 
 from flask import Flask, jsonify, request
@@ -19,11 +5,11 @@ import flask_cors
 from google.cloud import ndb
 from google.auth.transport import requests
 import google.oauth2.id_token
-import requests_toolbelt.adapters.appengine
+#import requests_toolbelt.adapters.appengine
 
 # Use the App Engine Requests adapter. This makes sure that Requests uses
 # URLFetch.
-requests_toolbelt.adapters.appengine.monkeypatch()
+#requests_toolbelt.adapters.appengine.monkeypatch()
 firebase_request_adapter = requests.Request()
 
 #HTTP_REQUEST = google.auth.transport.requests.Request()
